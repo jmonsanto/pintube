13081b8977a9440ecb366196713b7c0248f5b96a
John Enrico Monsanto <johnieee@Johns-MacBook-Pro.local>
2016-04-26 13:41:15 +0800

    Here be Pintube, the one app to rule them all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pintube::Application.config.secret_token = '751173dc0ead8b2bbfbb1090744718451dd82c32e0879050a612503127200b43c3220e82a82866957ce3fe680f1cc2fc72f1ca5fcdbbd8ae0e5c9e8bff85ccad'
