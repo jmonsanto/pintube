15b91e93585614a54eeb15f2c4fe64dada108ce0
John Enrico Monsanto <jhnrco.monsanto@gmail.com>
2016-04-30 18:07:49 +0800

    Set route constraints, namespacing and defaults for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Pintube::Application.routes.draw do
   root to: 'pages#index'
+
+  namespace '/api', defaults: { format: :json },
+                    constraints: { subdomain: 'api' }, path: '/' do
+  end
 end
