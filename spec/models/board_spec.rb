75e4ebb26dd014e18b7bbbcd5dcf57360b4529d6
John Enrico Monsanto <jhnrco.monsanto@gmail.com>
2016-05-01 07:08:41 +0800

    Add validation tests to existing board spec

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Board do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Board do
+    before { @board = FactoryGirl.build(:board) }
+
+    subject { @board }
+
+    # Attribute tests
+    it { should respond_to(:name) }
+    it { should respond_to(:description) }
+
+    # Validation tests
+    it { should be_valid }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_uniqueness_of(:name) }
+  end
 end
