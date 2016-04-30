c15073d11e04a914c9ba24ffad18f7fef2c7a902
John Enrico Monsanto <jhnrco.monsanto@gmail.com>
2016-04-30 23:37:23 +0800

    Implement API namespace and versioning
    Namespace needed so that API requests will be separated from the HTML
    requests. Also implemented versioning for backwards compatibility, in
    case after deployment, there are changes to the DB.
    
    This commit contains:
    * Update routes for namespacing and versioning of the API
    * Add tests for constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require 'api_constraints'
+
 Pintube::Application.routes.draw do
   root to: 'pages#index'
 
-  namespace '/api', defaults: { format: :json },
-                    constraints: { subdomain: 'api' }, path: '/' do
+  namespace '/api', defaults: { format: :json }, path: '/' do
+
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+    end
   end
 end
